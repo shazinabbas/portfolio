eabe7a29584a6dce9a4e9967a0e5fc30129c2028
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-05-05 21:16:23 +0530

    Add files via upload

---
+++ src/components/form.jsx
@@ -0,0 +1,22 @@
+import './form.css';
+
+export default function Form() {
+  return (
+    <form className="form">
+      <ul className="wrapper">
+        <li style={{ "--i": 4 }}>
+          <input className="input" type="text" placeholder="Name" required />
+        </li>
+        <li style={{ "--i": 3 }}>
+          <input className="input" placeholder="Phone number" required name="phone" />
+        </li>
+        <li style={{ "--i": 2 }}>
+          <input className="input" type="email" placeholder="E-mail" required name="email" />
+        </li>
+        <li style={{ "--i": 1 }}>
+          <button>Submit</button>
+        </li>
+      </ul>
+    </form>
+  );
+}
