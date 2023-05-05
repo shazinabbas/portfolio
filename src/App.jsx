607beef7e8d589191d05eea0d0e68a54fbbd04c0
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-05-05 22:10:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Tech />
       <Works />
       {/* <Feedbacks /> */}
-      <Form />
+      {/* <Form /> */}
       <div className="relative z-0">
           <Contact />
           <StarsCanvas />
