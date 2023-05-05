93c95d55329945cb296bf6ca8f5bebd2cb575f04
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-05-05 22:05:41 +0530

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,26 +1,49 @@
-import React from "react";
 import './form.css';
-import withFormValidation from './withFormValidation';
 
-function Form() {
+export default function Form() {
   return (
-    <form className="form">
-      <ul className="wrapper">
-        <li style={{ "--i": 4 }}>
-          <input className="input" type="text" placeholder="Name" required />
-        </li>
-        <li style={{ "--i": 3 }}>
-          <input className="input" placeholder="Phone number" required name="phone" />
-        </li>
-        <li style={{ "--i": 2 }}>
-          <input className="input" type="email" placeholder="E-mail" required name="email" />
-        </li>
-        <li style={{ "--i": 1 }}>
-          <button>Submit</button>
-        </li>
-      </ul>
+    <form className="form bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          Name
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          placeholder="Name"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="phone">
+          Phone Number
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="phone"
+          type="text"
+          placeholder="Phone Number"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
-
-export default withFormValidation(Form);
