feeefe774cbc8c71a63ab12b69219a692685cee3
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-05-31 23:01:47 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -39,7 +39,7 @@ const Ball = (props) => {
         />
         <OrbitControls 
           autoRotate
-          autoRotateSpeed={0.1}
+          autoRotateSpeed={0.01}
           reverseOrbit={false}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
