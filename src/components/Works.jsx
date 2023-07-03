40a6ab0cc9a66c58f0b00128586b4588eca19c5b
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-07-03 18:34:21 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,52 +2,81 @@ import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
  
 import { styles } from '../styles';
-import { github1 } from '../assets';
+import { figma, github1 } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
 
-const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+// const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+//   return (
+//     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+//       <Tilt
+//         options={{
+//           max: 45,
+//           scale: 1,
+//           speed: 450
+//         }}
+//         className="bg-tertiary p-3 sm:p-5 rounded-2xl w-full sm:w-[360px]"
+//       >
+//         <div className="relative w-full h-0 pb-[56.25%] sm:pb-[75%]">
+//           {/* Rest of the code */}
+//         </div>
+//         <div className="mt-3 sm:mt-5">
+//           <h3 className="text-white font-bold text-[20px] sm:text-[24px]">{name}</h3>
+//           <p className="mt-2 text-secondary text-[12px] sm:text-[14px] leading-[16px] sm:leading-[20px]">
+//             {description}
+//           </p>
+//         </div>
+//         <div className="mt-2 sm:mt-4 flex flex-wrap gap-1 sm:gap-2">
+//           {/* Rest of the code */}
+//         </div>
+//       </Tilt>
+//     </motion.div>
+//   );
+// };
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt 
+      <Tilt
         options={{
-          max: 45, 
-          scale: 1, 
+          max: 45,
+          scale: 1,
           speed: 450
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-3 sm:p-5 rounded-2xl w-full sm:w-[360px]"
       >
-        <div className="relative w-full h-[230px]">
-          <img 
+        <div className="relative w-full h-0 pb-[56.25%] sm:pb-[75%]">
+          <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover rounded-2xl"
+            className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl"
           />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
-              <img 
+              <img
                 src={github1}
-                alt={github1}
+                alt={figma}
                 className="w-1/2 h-1/2 object-contain"
-                />
+              />
             </div>
           </div>
         </div>
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}
+        <div className="mt-3 sm:mt-5">
+          <h3 className="text-white font-bold text-[20px] sm:text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[12px] sm:text-[14px] leading-[16px] sm:leading-[20px]">
+            {description}
           </p>
         </div>
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className="mt-2 sm:mt-4 flex flex-wrap gap-1 sm:gap-2">
           {tags.map((tag, index) => (
-            <p 
+            <p
               key={tag.name}
-              className={`text-[14px] ${tag.color}`}
+              className={`text-[12px] sm:text-[14px] ${tag.color}`}
             >
               #{tag.name}
             </p>
@@ -55,8 +84,8 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link})
         </div>
       </Tilt>
     </motion.div>
-  )
-}
+  );
+};
 
 const Works = () => {
   return (
@@ -79,15 +108,16 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="mt-20 flex flex-wrap gap-7 sm:gap-4">
         {projects.map((project, index) => (
-          <ProjectCard 
-            key={`project-${index}`}
-            index={index}
-            {...project}
-          />
-        ))}
-      </div>
+        <ProjectCard
+          key={`project-${index}`}
+          index={index}
+          {...project}
+        />
+  ))}
+</div>
+
     </>
   )
 }
