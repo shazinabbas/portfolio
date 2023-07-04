580074ecb7e843d39754065d996a00717969f959
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-07-04 13:41:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,68 @@ const App = () => {
       const isAtBottom = scrollPosition + windowHeight >= documentHeight;
 
       setShowBottomBar(isAtBottom);
+    };import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Experience,
+  BottomBar,
+} from "./components";
+
+const App = () => {
+  const [showBottomBar, setShowBottomBar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.scrollY;
+
+      const isAtBottom = scrollPosition + windowHeight >= documentHeight;
+
+      setShowBottomBar(isAtBottom);
     };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+          {showBottomBar && <BottomBar />}
+
+        </div>
+       
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
 
     window.addEventListener("scroll", handleScroll);
 
