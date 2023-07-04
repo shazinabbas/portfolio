a06294209cea74a2dab70f68892bba84b7208247
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-07-04 13:26:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, BottomBar } from "./components";
 
 
 const App = () => {
+  const [showBottomBar, setShowBottomBar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.scrollY;
+
+      const isAtBottom = scrollPosition + windowHeight >= documentHeight;
+
+      setShowBottomBar(isAtBottom);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  
   return (
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
@@ -17,6 +37,7 @@ const App = () => {
       <div className="relative z-0">
           <Contact />
           <StarsCanvas />
+        {showBottomBar && <BottomBar />}
       </div>
     </div>
     </BrowserRouter>
