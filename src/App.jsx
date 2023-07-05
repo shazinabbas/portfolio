536b5674a21a4d50c12670f9534ea88c85f9d773
Shaz'e <shazinabbas_m@srmap.edu.in>
2023-07-05 10:32:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,152 @@
-import React, { Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { useProgress } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+  About,
+  Contact,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Experience,
+  BottomBar,
+} from "./components";
+import { loader } from "./assets";
 
-import CanvasLoader from "../Loader";
-import { random } from "maath";
-
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
-
+const LoadingPage = () => {
   return (
-    <Float speed={0.75} rotationIntensity={1} floatIntensity={8}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-
-        <Decal
-          // autoRotate
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.2}
-          reverseOrbit={false}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-      </mesh>
-    </Float>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <video
+        autoPlay
+        loop
+        muted
+        style={{ maxWidth: "100%", maxHeight: "100%" }}
+      >
+        <source src={loader} type="video/mp4" />
+      </video>
+    </div>
   );
 };
 
-const BallCanvas = ({ icon }) => {
+const App = () => {
+  const [showBottomBar, setShowBottomBar] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { progress } = useProgress();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.scrollY;
+
+      const isAtBottom = scrollPosition + windowHeight >= documentHeight;
+
+      setShowBottomBar(isAtBottom);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (progress === 100) {
+      setIsLoading(false);
+    }
+  }, [progress]);
+
+  if (isLoading) {
+    return (
+      <>
+        <LoadingPage />
+        <Preload3DObjects onComplete={() => setIsLoading(false)} />
+      </>
+    );
+  }
+
   return (
-    <Canvas
-      frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [0, 0, 5.3], near: 0.1, far: 100 }}
-    >
-      <Suspense >
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-      <Preload all />
-    </Canvas>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+          {showBottomBar && <BottomBar />}
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
-export default BallCanvas;
+const Preload3DObjects = ({ onComplete }) => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+
+  useEffect(() => {
+    // List of 3D object file paths
+    const objectPaths = ["./desktop_pc/scene.gltf", "./planet/scene.gltf"];
+
+    const loader = new GLTFLoader();
+
+    const loadObject = (path) => {
+      return new Promise((resolve, reject) => {
+        loader.load(
+          path,
+          (gltf) => {
+            // Object loaded successfully
+            resolve(gltf);
+          },
+          (progress) => {
+            // Progress callback
+            setLoadingProgress((prevProgress) =>
+              Math.round(
+                ((prevProgress + progress.loaded) / objectPaths.length) * 100
+              )
+            );
+          },
+          (error) => {
+            // Error callback
+            reject(error);
+          }
+        );
+      });
+    };
+
+    // Load all objects
+    const loadAllObjects = async () => {
+      try {
+        const objectPromises = objectPaths.map((path) => loadObject(path));
+        const objects = await Promise.all(objectPromises);
+        
+        // Do something with the loaded objects if needed
+
+        onComplete();
+      } catch (error) {
+        console.error("Error loading 3D objects:", error);
+        // Handle error if needed
+      }
+    };
+
+    loadAllObjects();
+
+    // Cleanup
+    return () => {
+    };
+  }, []);
+
+  return null;
+};
+
+export default App;
