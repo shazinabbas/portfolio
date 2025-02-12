a8f39dc5967ba95e099227fce94f71f901e91f07
Shazinabbas <shazinabbas_m@srmap.edu.in>
2025-02-12 22:40:57 +0530

    Enhance Hero component layout with additional styling and text elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,20 @@ const Hero = () => {
   return (
 
     <section className="relative w-full h-screen mx-auto">
+      
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-end items-center px-60 py-48">
+          <h1 className={`${styles.sectionSubText} text-white`}>
+            <span className="text-[#31BB69]">Pull it!</span>
+          </h1>
+        </div>
+        
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#31BB69]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi👋🏻, I'm <span className="text-[#31BB69]">Shazin</span>
@@ -21,8 +30,12 @@ const Hero = () => {
           Associate Software Engineer Intern <br className="sm:block hidden" />Kaleris
           </p>
         </div>
-        </div>
-        <Badge />
+
+      </div>
+
+      <Badge />
+      
+      
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
@@ -40,6 +53,9 @@ const Hero = () => {
           </div>
         </a>
       </div>
+
+
+      
     </section>
   )
 }
